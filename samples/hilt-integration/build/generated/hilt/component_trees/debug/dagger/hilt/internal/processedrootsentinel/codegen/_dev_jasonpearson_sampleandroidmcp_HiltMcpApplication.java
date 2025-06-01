da2518afcc92868d735b3a9d0b81ed7210c0c262
Jason Pearson <jason.d.pearson@gmail.com>
2025-06-01 02:18:55 -0500

    ignore build folders

--- samples/hilt-integration/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_dev_jasonpearson_sampleandroidmcp_HiltMcpApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "dev.jasonpearson.sampleandroidmcp.HiltMcpApplication"
-)
-public final class _dev_jasonpearson_sampleandroidmcp_HiltMcpApplication {
-}
